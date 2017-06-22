f9cbd57a6a81c12d42db22ae045efef5cdd4523d
Dirk Lammers <di.lammers@lvm.de>
2017-06-22 16:26:36 +0200

    sentry integration, added test logging

--- src/app/logview.component.ts
+++ src/app/logview.component.ts
@@ -7,16 +7,15 @@ import { Logger } from './core/logger.service';
  */
 @Component({
     selector: 'app-root',
-    template: `<h1>
-                {{title}}
-               </h1>`
+    templateUrl: './logview.component.html'
 })
 export class LogviewComponent implements OnInit {
-
 
     title = 'Logger Configuration Page';
 
     private log: Logger;
+
+    public msg = 'test Message to Log';
 
     constructor(private logManager: LogManager) {
         this.log = logManager.getLogger(this, '#4863A0');
@@ -29,6 +28,34 @@ export class LogviewComponent implements OnInit {
 
     logIt(): boolean {
         return this.log.info('logged');
+    }
+
+    logError() {
+        this.log.error(this.msg);
+    }
+
+    logVerbose() {
+        this.log.verbose(this.msg);
+    }
+
+    logDebug() {
+        this.log.debug(this.msg);
+    }
+
+    logInfo() {
+        this.log.info(this.msg);
+    }
+
+    logWarn() {
+        this.log.warn(this.msg);
+    }
+
+    logCritical() {
+        this.log.critical(this.msg);
+    }
+
+    throwError() {
+        throw new Error(this.msg);
     }
 
 }
--- src/app/logview.module.ts
+++ src/app/logview.module.ts
@@ -1,7 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { LoggingModule } from './index';
 import { LogviewComponent } from './logview.component';
+import * as Raven from 'raven-js';
+import { FormsModule } from '@angular/forms';
+
+Raven
+    .config('https://78de191c783e4309b83fba3f8abcde5b@sentry.io/182689')
+    .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+    handleError(err: any): void {
+        Raven.captureException(err.originalError);
+    }
+}
 
 @NgModule({
     declarations: [
@@ -9,10 +21,10 @@ import { LogviewComponent } from './logview.component';
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         LoggingModule
     ],
-    providers: [
-    ],
+    providers: [{provide: ErrorHandler, useClass: RavenErrorHandler}],
     bootstrap: [LogviewComponent]
 })
 /**
